f056b52ef11f8fbb6d3d8236a2fc1b9790519893
luke <694014820@qq.com>
2025-12-13 23:13:52 +0800

    优化ContainerProcess

--- daemon/src/main/java/com/github/adocker/daemon/containers/ContainerProcess.kt
+++ daemon/src/main/java/com/github/adocker/daemon/containers/ContainerProcess.kt
@@ -1,23 +1,80 @@
 package com.github.adocker.daemon.containers
 
+import android.os.Looper
+import android.os.MessageQueue.OnFileDescriptorEventListener
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runInterruptible
+import kotlinx.coroutines.suspendCancellableCoroutine
+import timber.log.Timber
+import java.io.FileDescriptor
+import java.io.FileOutputStream
+import java.io.FilterOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 import javax.inject.Singleton
+import kotlin.coroutines.resume
 
 class ContainerProcess @AssistedInject constructor(
     @Assisted
     private val process: Process,
     scope: CoroutineScope
 ) {
+
+    companion object {
+        private val getFileDescriptor by lazy<Result<(FilterOutputStream) -> FileDescriptor>> {
+            runCatching {
+                FilterOutputStream::class.java.getDeclaredField("out").apply {
+                    isAccessible = true
+                }
+            }.map { field ->
+                return@map { stream ->
+                    field.get(stream).let {
+                        it as FileOutputStream
+                    }.fd
+                }
+            }
+        }
+    }
+
     val job = scope.launch {
         try {
+            suspendCancellableCoroutine { con ->
+                getFileDescriptor.mapCatching {
+                    it(process.outputStream as FilterOutputStream)
+                }.fold(
+                    { fd ->
+                        val queue = Looper.getMainLooper().queue
+                        val callbacks = object : OnFileDescriptorEventListener, CompletionHandler {
+                            override fun onFileDescriptorEvents(
+                                fd: FileDescriptor,
+                                events: Int
+                            ): Int {
+                                con.resume(Unit)
+                                return 0
+                            }
+
+                            override fun invoke(p1: Throwable?) {
+                                queue.removeOnFileDescriptorEventListener(fd)
+                            }
+                        }
+                        queue.addOnFileDescriptorEventListener(
+                            fd,
+                            OnFileDescriptorEventListener.EVENT_ERROR,
+                            callbacks
+                        )
+                    },
+                    {
+                        Timber.d(it)
+                        con.resume(Unit)
+                    }
+                )
+            }
             runInterruptible {
                 process.waitFor()
             }
