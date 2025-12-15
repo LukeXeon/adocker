b3eaddd1ce0ab0996d518c9df3c032631027a527
晨曦 <luotinghui@bilibili.com>
2025-12-15 16:00:09 +0800

    重构

--- daemon/src/main/java/com/github/adocker/daemon/containers/ContainerProcess.kt
+++ daemon/src/main/java/com/github/adocker/daemon/containers/ContainerProcess.kt
@@ -8,9 +8,11 @@ import dagger.assisted.AssistedInject
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CompletionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runInterruptible
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.io.FileDescriptor
 import java.io.FileOutputStream
@@ -76,8 +78,10 @@ class ContainerProcess @AssistedInject constructor(
                     }
                 )
             }
-            runInterruptible {
-                process.waitFor()
+            withContext(Dispatchers.IO) {
+                runInterruptible {
+                    process.waitFor()
+                }
             }
         } catch (e: CancellationException) {
             process.destroy()
