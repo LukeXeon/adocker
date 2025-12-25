2eacd26e410859c5f897439af09468fabf49f155
晨曦 <luotinghui@bilibili.com>
2025-12-25 12:54:08 +0800

    重构

--- app/src/main/java/com/github/andock/ui/screens/containers/ContainerStateChip.kt
+++ app/src/main/java/com/github/andock/ui/screens/containers/ContainerStateChip.kt
@@ -1,10 +1,14 @@
 package com.github.andock.ui.screens.containers
 
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.github.andock.daemon.containers.ContainerState
 
 @Composable
@@ -19,11 +23,13 @@ fun ContainerStateChip(state: ContainerState) {
         is ContainerState.Removing -> MaterialTheme.colorScheme.error
         is ContainerState.Removed -> MaterialTheme.colorScheme.error
     }
-    AssistChip(
-        onClick = { },
-        label = { Text(state::class.simpleName ?: "") },
-        colors = AssistChipDefaults.assistChipColors(
-            labelColor = color
-        )
+    Text(
+        state::class.simpleName ?: "",
+        style = MaterialTheme.typography.labelSmall,
+        maxLines = 1,
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(color)
+            .padding(horizontal = 4.dp)
     )
 }
