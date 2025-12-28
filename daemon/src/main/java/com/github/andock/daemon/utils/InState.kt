66c611b92065c86904da3f63a8d24675ed5b57b0
luke <694014820@qq.com>
2025-12-29 05:18:53 +0800

    重构

--- daemon/src/main/java/com/github/andock/daemon/utils/InState.kt
+++ daemon/src/main/java/com/github/andock/daemon/utils/InState.kt
@@ -5,43 +5,41 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import javax.inject.Provider
 import kotlin.reflect.KClass
 
 typealias InState<T> = Pair<StateFlow<T>, Set<KClass<out T>>>
 
+interface Collector : Provider<Any?>, suspend (Boolean) -> Unit
 @PublishedApi
-internal abstract class Collector(
-    private val inState: InState<*>,
-) : CancellationException(
-    "Flow was aborted, no more elements needed"
-), suspend (Boolean) -> Unit {
-
-    var result: Any? = this
-
-    override fun fillInStackTrace(): Throwable {
-        stackTrace = emptyArray()
-        return this
-    }
-
-    override suspend fun invoke(value: Boolean) {
-        if (value) {
-            result = execute()
-            throw this
-        } else {
-            throw IllegalStateException("${inState.first} is not ${inState.second}")
-        }
-    }
-
-    abstract suspend fun execute(): Any?
-}
-
-@PublishedApi
-internal inline fun Collector(
-    inState: InState<*>,
+internal inline fun InState<*>.collector(
     crossinline block: suspend () -> Any?
 ): Collector {
-    return object : Collector(inState) {
-        override suspend fun execute() = block()
+    return object : CancellationException(
+        "Flow was aborted, no more elements needed"
+    ), Collector {
+        private var value: Any? = this
+
+        override fun fillInStackTrace(): Throwable {
+            stackTrace = emptyArray()
+            return this
+        }
+
+        override suspend fun invoke(value: Boolean) {
+            if (value) {
+                this.value = block()
+                throw this
+            } else {
+                throw IllegalStateException("$first is not $second")
+            }
+        }
+
+        override fun get(): Any? {
+            if (value == this) {
+                throw NoSuchElementException("Expected at least one element")
+            }
+            return value
+        }
     }
 }
 
@@ -49,7 +47,7 @@ suspend inline fun <reified R> InState<*>.execute(
     crossinline block: suspend () -> R
 ): R {
     val (state, classes) = this
-    val collector = Collector(this) {
+    val collector = collector {
         block()
     }
     try {
@@ -58,15 +56,12 @@ suspend inline fun <reified R> InState<*>.execute(
                 clazz.isInstance(state)
             }
         }.distinctUntilChanged().collectLatest(collector)
-    } catch (e: Collector) {
+    } catch (e: CancellationException) {
         if (e != collector) {
             throw e
         }
     }
-    if (collector.result == collector) {
-        throw NoSuchElementException("Expected at least one element")
-    }
-    return collector.result as R
+    return collector.get() as R
 }
 
 fun <T : Any> StateFlow<T>.inState(vararg classes: KClass<out T>): InState<T> {
