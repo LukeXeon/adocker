da32f0125d930d9fe7b3159bfb9b2ff750bfff8a
luke <694014820@qq.com>
2026-01-05 03:02:21 +0800

    重构

--- daemon/src/main/java/com/github/andock/daemon/os/RemoteProcessBuilderService.kt
+++ daemon/src/main/java/com/github/andock/daemon/os/RemoteProcessBuilderService.kt
@@ -6,22 +6,18 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import timber.log.Timber
+import kotlin.coroutines.CoroutineContext
 import kotlin.system.exitProcess
 
-class RemoteProcessBuilderService : IRemoteProcessBuilderService.Stub() {
+class RemoteProcessBuilderService : IRemoteProcessBuilderService.Stub(),
+    CoroutineExceptionHandler {
 
     init {
         Timber.plant(Timber.DebugTree())
     }
 
     private val scope = CoroutineScope(
-        SupervisorJob()
-                + Dispatchers.IO
-                + CoroutineExceptionHandler { _, e ->
-            if (e !is CancellationException) {
-                Timber.d(e)
-            }
-        }
+        SupervisorJob() + Dispatchers.IO + this
     )
 
     override fun newProcess(
@@ -35,4 +31,18 @@ class RemoteProcessBuilderService : IRemoteProcessBuilderService.Stub() {
     override fun destroy() {
         exitProcess(0)
     }
+
+    @Deprecated("Coroutine use only", level = DeprecationLevel.HIDDEN)
+    override fun handleException(
+        context: CoroutineContext,
+        exception: Throwable
+    ) {
+        if (exception !is CancellationException) {
+            Timber.d(exception)
+        }
+    }
+
+    @Deprecated("Coroutine use only", level = DeprecationLevel.HIDDEN)
+    override val key: CoroutineContext.Key<*>
+        get() = CoroutineExceptionHandler
 }
