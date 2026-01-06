9cdb9959983bfeee5ee2b3c5ece5865e4f0401bb
luke <694014820@qq.com>
2026-01-06 22:07:02 +0800

    重构

---
+++ daemon/src/main/java/com/github/andock/daemon/app/AppTask.kt
@@ -0,0 +1,5 @@
+package com.github.andock.daemon.app
+
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+annotation class AppTask(val name: String)
