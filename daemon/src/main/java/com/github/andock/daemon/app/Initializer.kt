2c226c7812cf63388f679b5216e844bcf02a929f
luke <694014820@qq.com>
2026-01-07 02:52:24 +0800

    重构

--- daemon/src/main/java/com/github/andock/daemon/app/Initializer.kt
+++ daemon/src/main/java/com/github/andock/daemon/app/Initializer.kt
@@ -2,6 +2,8 @@ package com.github.andock.daemon.app
 
 import android.app.Application
 import com.github.andock.daemon.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.acra.config.mailSender
 import org.acra.data.StringFormat
 import org.acra.ktx.initAcra
@@ -21,24 +23,26 @@ fun appContext(
 }
 
 @AppTask("reporter")
-fun crashReporter(
+suspend fun crashReporter(
     @Suppress("unused")
     @AppTask("logging")
     logging: Unit,
     application: Application
 ) {
-    application.initAcra {
-        buildConfigClass = sequenceOf(
-            application.packageName,
-            "${application.packageName}.daemon"
-        ).mapNotNull {
-            runCatching {
-                Class.forName("${it}.BuildConfig")
-            }.getOrNull()
-        }.firstOrNull() ?: throw AssertionError("No found BuildConfig class")
-        reportFormat = StringFormat.JSON
-        mailSender {
-            mailTo = application.getString(R.string.qq_email)
+    withContext(Dispatchers.IO) {
+        application.initAcra {
+            buildConfigClass = sequenceOf(
+                application.packageName,
+                "${application.packageName}.daemon"
+            ).mapNotNull {
+                runCatching {
+                    Class.forName("${it}.BuildConfig")
+                }.getOrNull()
+            }.firstOrNull() ?: throw AssertionError("No found BuildConfig class")
+            reportFormat = StringFormat.JSON
+            mailSender {
+                mailTo = application.getString(R.string.qq_email)
+            }
         }
     }
 }
