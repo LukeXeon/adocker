2e2d5c8e1a1d65838bfc272b7a7a7864db4ca929
晨曦 <luotinghui@bilibili.com>
2026-01-08 19:20:31 +0800

    重构

--- daemon/src/main/java/com/github/andock/daemon/utils/InState.kt
+++ daemon/src/main/java/com/github/andock/daemon/utils/InState.kt
@@ -1,55 +1,36 @@
 package com.github.andock.daemon.utils
 
-import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
 
 typealias InState<T> = Pair<StateFlow<T>, Set<KClass<out T>>>
 
-@PublishedApi
-internal suspend inline fun executeImpl(
-    state: StateFlow<*>,
-    classes: Set<KClass<*>>,
-    crossinline block: suspend () -> Any?
-): Any? {
-    val collector = object : CancellationException(
-        "Flow was aborted, no more elements needed"
-    ), suspend (Boolean) -> Unit {
-        var result: Any? = this
-
-        override fun fillInStackTrace(): Throwable {
-            stackTrace = emptyArray()
-            return this
-        }
-
-        override suspend fun invoke(value: Boolean) {
-            check(value) { "$state is not $classes" }
-            result = block()
-            throw this
-        }
-    }
-    try {
-        state.map { state ->
-            classes.any { clazz ->
-                clazz.isInstance(state)
-            }
-        }.distinctUntilChanged().collectLatest(collector)
-    } catch (e: CancellationException) {
-        if (e != collector) {
-            throw e
-        }
-    }
-    check(collector.result != collector)
-    return collector.result
-}
-
 suspend inline fun <reified R> InState<*>.execute(
     crossinline block: suspend () -> R
 ): R {
-    return executeImpl(first, second, block) as R
+    val (state, classes) = this
+    return coroutineScope {
+        val job = async {
+            block()
+        }
+        launch {
+            state.map { state ->
+                classes.any { clazz ->
+                    clazz.isInstance(state)
+                }
+            }.distinctUntilChanged().collect {
+                if (!it) {
+                    job.cancel()
+                }
+            }
+        }
+        job.await()
+    }
 }
 
 fun <T : Any> StateFlow<T>.inState(vararg classes: KClass<out T>): InState<T> {
