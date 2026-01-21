83735912419d045bcab1ca90d22b9eb32eb9a86f
luotinghui <luotinghui@bilibili.com>
2026-01-21 20:12:08 +0800

    重构

--- startup/src/main/java/com/github/andock/startup/Startup.kt
+++ startup/src/main/java/com/github/andock/startup/Startup.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Looper
 import androidx.annotation.MainThread
 import dagger.hilt.android.EntryPointAccessors
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -20,6 +21,7 @@ fun Context.trigger(
     val batch = entrypoint.newInstance(key)
     val job = GlobalScope.async(
         context = Dispatchers.Main.immediate,
+        start = CoroutineStart.UNDISPATCHED,
         block = batch
     )
     try {
