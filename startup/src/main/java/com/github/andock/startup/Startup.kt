48a88a6e1f6ef54f03e40c95d4ad819e1fb3a2c3
luotinghui <luotinghui@bilibili.com>
2026-01-22 20:24:54 +0800

    重构

--- startup/src/main/java/com/github/andock/startup/Startup.kt
+++ startup/src/main/java/com/github/andock/startup/Startup.kt
@@ -4,11 +4,8 @@ import android.content.Context
 import android.os.Looper
 import androidx.annotation.MainThread
 import dagger.hilt.android.EntryPointAccessors
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 @OptIn(DelicateCoroutinesApi::class)
 @MainThread
@@ -19,13 +16,7 @@ fun Context.trigger(
     val entrypoint = EntryPointAccessors.fromApplication<TaskBatchFactory>(this)
     val batch = entrypoint.newInstance(key)
     return runBlocking(
-        context = StartupRootContext
-    ) {
-        withContext(
-            context = RedirectDispatchers(
-                mainDispatcher = coroutineContext[CoroutineDispatcher]!!
-            ) + Dispatchers.Main.immediate,
-            block = batch
-        )
-    }
+        context = StartupRootContext,
+        block = batch
+    )
 }
--- startup/src/main/java/com/github/andock/startup/TaskBatch.kt
+++ startup/src/main/java/com/github/andock/startup/TaskBatch.kt
@@ -3,10 +3,13 @@ package com.github.andock.startup
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.withContext
 import javax.inject.Singleton
 
 internal class TaskBatch @AssistedInject constructor(
@@ -15,15 +18,32 @@ internal class TaskBatch @AssistedInject constructor(
     @param:InternalName("tasks")
     private val tasks: @JvmSuppressWildcards Map<String, List<Pair<String, TaskComputeTime>>>,
 ) : suspend (CoroutineScope) -> List<TaskResult> {
+    private var isEntered: Boolean = false
 
     override suspend fun invoke(scope: CoroutineScope): List<TaskResult> {
-        val tasks = tasks.getValue(key)
-        return tasks.map { task ->
-            scope.async(Dispatchers.Default) {
-                val (phaseTime, totalTime) = task.second()
-                TaskResult(task.first, phaseTime, totalTime)
+        if (isEntered) {
+            val tasks = tasks.getValue(key)
+            return tasks.map { task ->
+                scope.async(Dispatchers.Default) {
+                    val (phaseTime, totalTime) = task.second()
+                    TaskResult(task.first, phaseTime, totalTime)
+                }
+            }.awaitAll()
+        } else {
+            try {
+                isEntered = true
+                val context = currentCoroutineContext()
+                val mainDispatcher = checkNotNull(context[CoroutineDispatcher]) {
+                    "not found CoroutineDispatcher"
+                }
+                return withContext(
+                    context = RedirectDispatchers(mainDispatcher) + Dispatchers.Main.immediate,
+                    block = this
+                )
+            } finally {
+                isEntered = false
             }
-        }.awaitAll()
+        }
     }
 
     @Singleton
