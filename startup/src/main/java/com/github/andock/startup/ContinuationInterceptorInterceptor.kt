c48270dc4ee8b9d7433ccd4e1e98ac739289be68
luotinghui <luotinghui@bilibili.com>
2026-01-22 17:29:30 +0800

    重构

--- startup/src/main/java/com/github/andock/startup/ContinuationInterceptorInterceptor.kt
+++ startup/src/main/java/com/github/andock/startup/ContinuationInterceptorInterceptor.kt
@@ -3,7 +3,7 @@ package com.github.andock.startup
 import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 
-interface ContinuationInterceptorInterceptor : CoroutineContext.Element {
+internal interface ContinuationInterceptorInterceptor : CoroutineContext.Element {
 
     fun intercept(interceptor: ContinuationInterceptor): ContinuationInterceptor
 
