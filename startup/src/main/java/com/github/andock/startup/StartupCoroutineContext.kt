529bad6fb42f4612fb7f2d4e823f378706aaad1a
luotinghui <luotinghui@bilibili.com>
2026-01-22 17:22:27 +0800

    重构

--- startup/src/main/java/com/github/andock/startup/StartupCoroutineContext.kt
+++ startup/src/main/java/com/github/andock/startup/StartupCoroutineContext.kt
@@ -6,13 +6,15 @@ import kotlin.coroutines.EmptyCoroutineContext
 
 interface StartupCoroutineContext : CoroutineContext {
     override fun plus(context: CoroutineContext): CoroutineContext {
-        return if (this === StartupRootContext) {
+        return if (context === EmptyCoroutineContext) {
             // fast path -- avoid lambda creation
             this
         } else {
             context.fold<CoroutineContext>(this) { acc, element ->
                 val removed = acc.minusKey(element.key)
-                if (removed === EmptyCoroutineContext) element else {
+                if (removed === EmptyCoroutineContext) {
+                    element
+                } else {
                     // make sure interceptor is always last in the context (and thus is fast to get when present)
                     val interceptor = removed[ContinuationInterceptor]
                     if (interceptor == null) {
@@ -22,7 +24,10 @@ interface StartupCoroutineContext : CoroutineContext {
                         if (left === EmptyCoroutineContext) {
                             StartupCombinedContext(element, interceptor)
                         } else {
-                            StartupCombinedContext(StartupCombinedContext(left, element), interceptor)
+                            StartupCombinedContext(
+                                StartupCombinedContext(left, element),
+                                interceptor
+                            )
                         }
                     }
                 }
