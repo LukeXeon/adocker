7dc464f179e2e1be12271ae4c3c2183d424c9326
luotinghui <luotinghui@bilibili.com>
2026-01-23 15:15:56 +0800

    重构

--- startup/src/main/java/com/github/andock/startup/DispatcherType.kt
+++ startup/src/main/java/com/github/andock/startup/DispatcherType.kt
@@ -1,15 +1,8 @@
 package com.github.andock.startup
 
-import android.os.Handler
-import android.os.HandlerThread
-import android.os.Process
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.android.asCoroutineDispatcher
-import kotlinx.coroutines.asCoroutineDispatcher
-import java.util.concurrent.ForkJoinPool
 import javax.inject.Provider
-import android.os.AsyncTask as AndroidAsyncTask
 
 enum class DispatcherType : Provider<CoroutineDispatcher> {
     Main {
@@ -25,46 +18,12 @@ enum class DispatcherType : Provider<CoroutineDispatcher> {
         override fun get() = Dispatchers.IO
     },
     ForkJoin {
-        private val value by lazy {
-            ForkJoinPool.commonPool().asCoroutineDispatcher()
-        }
-
-        override fun get(): CoroutineDispatcher {
-            return value
-        }
+        override fun get() = Dispatchers.ForkJoin
     },
     AsyncTask {
-        private val value by lazy {
-            AndroidAsyncTask.THREAD_POOL_EXECUTOR.asCoroutineDispatcher()
-        }
-
-        override fun get(): CoroutineDispatcher {
-            return value
-        }
+        override fun get() = Dispatchers.AsyncTask
     },
     QueuedWork {
-        private val value by lazy {
-            runCatching {
-                Class.forName("android.os.QueuedWork")
-                    .getDeclaredMethod("getHandler").apply {
-                        isAccessible = true
-                    }.invoke(null) as Handler
-            }.map {
-                it.looper
-            }.recover {
-                HandlerThread(
-                    "queued-work-looper",
-                    Process.THREAD_PRIORITY_FOREGROUND
-                ).apply {
-                    start()
-                }.looper
-            }.map {
-                Handler(it).asCoroutineDispatcher()
-            }.getOrThrow()
-        }
-
-        override fun get(): CoroutineDispatcher {
-            return value
-        }
+        override fun get() = Dispatchers.QueuedWork
     }
 }
---
+++ startup/src/main/java/com/github/andock/startup/ExtraDispatchers.kt
@@ -0,0 +1,42 @@
+package com.github.andock.startup
+
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Process
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.android.asCoroutineDispatcher
+import kotlinx.coroutines.asCoroutineDispatcher
+import java.util.concurrent.ForkJoinPool
+
+
+private const val TAG = "ExtraDispatchers"
+
+val Dispatchers.QueuedWork by lazy {
+    runCatching {
+        Class.forName("android.os.QueuedWork")
+            .getDeclaredMethod("getHandler").apply {
+                isAccessible = true
+            }.invoke(null) as Handler
+    }.map {
+        it.looper
+    }.recover {
+        Log.e(TAG, "access android.os.QueuedWork failed", it)
+        HandlerThread(
+            "queued-work-looper",
+            Process.THREAD_PRIORITY_FOREGROUND
+        ).apply {
+            start()
+        }.looper
+    }.map {
+        Handler(it).asCoroutineDispatcher()
+    }.getOrThrow()
+}
+
+val Dispatchers.AsyncTask by lazy {
+    android.os.AsyncTask.THREAD_POOL_EXECUTOR.asCoroutineDispatcher()
+}
+
+val Dispatchers.ForkJoin by lazy {
+    ForkJoinPool.commonPool().asCoroutineDispatcher()
+}
