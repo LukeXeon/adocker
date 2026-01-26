247e13952495c061aaa0176debd4d891bf7dce82
luotinghui <luotinghui@bilibili.com>
2026-01-26 17:31:21 +0800

    重构

--- startup/src/main/java/com/github/andock/startup/AndroidXTrigger.kt
+++ startup/src/main/java/com/github/andock/startup/AndroidXTrigger.kt
@@ -1,13 +1,11 @@
 package com.github.andock.startup
 
 import android.content.Context
-import androidx.annotation.RestrictTo
 import androidx.startup.Initializer
 import androidx.startup.R
 
 
-@RestrictTo(RestrictTo.Scope.LIBRARY)
-class AndroidXTrigger : Initializer<Stats> {
+internal class AndroidXTrigger : Initializer<Stats> {
 
     override fun create(context: Context): Stats {
         return context.trigger(context.resources.getString(R.string.androidx_startup))
