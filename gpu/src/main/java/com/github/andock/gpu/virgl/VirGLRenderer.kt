14647eeb577b4827c5a2e974e9902e4c9cd6d654
luke <694014820@qq.com>
2026-01-31 00:25:08 +0800

    重构

--- gpu/src/main/java/com/github/andock/gpu/virgl/VirGLRenderer.kt
+++ gpu/src/main/java/com/github/andock/gpu/virgl/VirGLRenderer.kt
@@ -1,13 +1,24 @@
 package com.github.andock.gpu.virgl
 
 import android.graphics.SurfaceTexture
+import android.net.LocalSocket
 import android.view.Surface
+import java.io.Closeable
 
 class VirGLRenderer(
-    val id: Long
-) {
+    val id: Long,
+    val localSocket: LocalSocket,
+) : Closeable {
     val surfaceTexture = SurfaceTexture(false)
     private val surface = Surface(surfaceTexture)
+
+    override fun close() {
+
+    }
+
+    init {
+        surfaceTexture.setDefaultBufferSize(1280, 720)
+    }
 
 
 }
--- gpu/src/main/java/com/github/andock/gpu/virgl/VirGLRendererServer.kt
+++ gpu/src/main/java/com/github/andock/gpu/virgl/VirGLRendererServer.kt
@@ -1,17 +1,41 @@
 package com.github.andock.gpu.virgl
 
+import android.app.Application
+import android.net.LocalServerSocket
+import android.net.LocalSocket
+import android.net.LocalSocketAddress
 import androidx.collection.MutableLongObjectMap
+import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class VirGLRendererServer @Inject constructor() {
+class VirGLRendererServer @Inject constructor(
+    application: Application
+) {
 
     init {
         System.loadLibrary("virgl_jni")
     }
 
     private val clients = MutableLongObjectMap<VirGLRenderer>()
+
+    val path = File(
+        application.cacheDir,
+        "virglrenderer_pipe"
+    )
+
+    private val localSocket = LocalSocket()
+    private val localServerSocket = LocalServerSocket(
+        localSocket.apply {
+            localSocket.bind(
+                LocalSocketAddress(
+                    path.absolutePath,
+                    LocalSocketAddress.Namespace.FILESYSTEM
+                )
+            )
+        }.fileDescriptor
+    )
 
     fun start() {
 
