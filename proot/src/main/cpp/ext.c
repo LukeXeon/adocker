7f5d596effc007a1bd9b50120eddd55078461ee0
luotinghui <luotinghui@bilibili.com>
2026-02-05 19:20:52 +0800

    修复编译

--- proot/src/main/cpp/ext.c
+++ proot/src/main/cpp/ext.c
@@ -5,8 +5,7 @@
 // Created by 罗庭辉 on 2026/2/5.
 //
 
-extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_github_andock_proot_PRoot_getVersion(JNIEnv *env, jclass thiz) {
-    return env->NewStringUTF(VERSION);
+    return (*env)->NewStringUTF(env, VERSION);
 }
